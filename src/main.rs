0e50111b3cea4399fc73e31f08a50f0757f68a0c
postBG <profile2697@gmail.com>
2021-12-27 17:08:47 +0900

    refactor: add parse config

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let (query, filename) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
@@ -17,4 +16,16 @@ fn main() {
     f.read_to_string(&mut contents).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let qurey = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
